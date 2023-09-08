f998efd7b5d1943a89264214082fc6754610150b
sunshine-web-coder <webcoderdev02@gmail.com>
2023-09-08 07:31:59 +0100

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,13 +4,13 @@ export default function Footer() {
   return (
     <div className="pt-[80px] pb-[80px] bg-[#030734]">
       <div className="custom_container flex gap-5">
-        <div className="w-2/6">
+        <div className="w-full md:w-2/6">
           <div><img src="https://i.imgur.com/xJ29aEg.png" alt="" /></div>
           <p className="text-[hsla(0,0%,100%,.8)] text-base mt-[27px] mb-[30px]"> On the other hand, we denounce whteous indig nation and dislike men wh beguiled moraized er hand consec teturus adipis iscing elit eiusmod tempordunt labore dolore magna aliqua consector tetur adip iscing. </p>
         </div>
-        <div className="w-3/12"></div>
-        <div className="w-2/12"></div>
-        <div className="w-3/12"></div>
+        <div className="w-full md:w-3/12"></div>
+        <div className="w-full md:w-2/12"></div>
+        <div className="w-full md:w-3/12"></div>
       </div>
     </div>
   );
